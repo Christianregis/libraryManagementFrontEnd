16b97ea14ac404dc8bd42c2e7ef4f396e239a745
Chritianregis <christiancheukap@gmailcom>
2026-03-07 02:18:38 +0100

    Add member page

---
+++ src/app/core/service/member-service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+
+  constructor(private http: HttpClient){}
+
+  private baseUrl = 'http://localhost:8080/api/member';
+
+  getAllBooks(){
+    return this.http.get(`${this.baseUrl}/books`)
+  }
+}
